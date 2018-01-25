b6e39557e83ffbd27669d4e93a08425a2209613f
borisgalicky <boris.galicky@akademiasovy.sk>
2018-01-25 13:00:57 +0100

    added error message and new features

--- script.js
+++ script.js
@@ -2,41 +2,37 @@ $(document).ready(function(){
     $("#get_button").click(function(){
         var city=$("#city").val();
         var code=$("#country_code").val();
-        if(city.length>1)
-        {
+        if(city.length>1){
         $("#err_city").html("");
         var link='http://api.openweathermap.org/data/2.5/weather?q=';
         link=link+city;
         }
-        else
-        {
+        else{
         $("#err_city").html("Incorrect city name!");
         }
-        if(code.length==2)
-        {
+        if(code.length==2){
         link=link+","+code;
         $("#err_code").html("");
         link=link+"&appid=8563a96a072dd0e90144a4a4b7e108cf";
         }
-        else
-        {
+        else{
         $("#err_code").html("Incorrect code!");
         }
   
         $.ajax({
             url:link,
             data: {format: 'json'},
-            error : function(){},
+            error : function(){
+            $("#content").html("<div id='err_msg'><b>Error has occured!</b><br>Weather service is not available right now.</br><b>We're sorry</b> for inconvenience.</div>");
+            },
             dataType: 'json',
             success : function(data){
-                console.log('temp: '+data.main.temp);
-                console.log('desc: '+data.weather[0].description);
                 $('#content').empty();
                 var table = $("<table/>");
                 table.attr("id","data_table");
                 var tr=$("<tr/>");
                 var td1=$("<td/>");
-                $(td1).append("City:");
+                $(td1).append("<b>City: </b>");
                 var td2=$("<td/>");
                 $(td2).append(city);
                 tr.append(td1);
@@ -45,18 +41,39 @@ $(document).ready(function(){
                 
                 var tr=$("<tr/>");
                 var td1=$("<td/>");
-                $(td1).append("Temperature:");
+                $(td1).append("<b>Temperature: </b>");
                 var td2=$("<td/>");
-                $(td2).append(data.main.temp-273.15);
+                $(td2).append(Math.floor(data.main.temp-273.15)+"°C");
                 tr.append(td1);
                 tr.append(td2);
                 table.append(tr);
                 
                 var tr=$("<tr/>");
                 var td1=$("<td/>");
-                $(td1).append("Humidity:");
+                $(td1).append("<b>Humidity: </b>");
                 var td2=$("<td/>");
-                $(td2).append(data.main.temp-273.15);
+                $(td2).append(data.main.humidity+"%");
+                tr.append(td1);
+                tr.append(td2);
+                table.append(tr);
+                
+                var tr=$("<tr/>");
+                var td1=$("<td/>");
+                $(td1).append("<b>Description: </b>");
+                var td2=$("<td/>");
+                $(td2).append(data.weather[0].description);
+                if(data.weather[0].description=="mist"){
+                td2.html("(not available)");
+                }
+                tr.append(td1);
+                tr.append(td2);
+                table.append(tr);
+                
+                var tr=$("<tr/>");
+                var td1=$("<td/>");
+                $(td1).append("<b>Pressure: </b>");
+                var td2=$("<td/>");
+                $(td2).append(data.main.pressure+" hPa");
                 tr.append(td1);
                 tr.append(td2);
                 table.append(tr);
